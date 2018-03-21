c7933c07f9ee81fabda183615aaa9748af303c62
one-atom <519001901@qq.com>
2018-03-21 01:09:34 -0400

    code

--- SBUSocks/client.py
+++ SBUSocks/client.py
@@ -2,11 +2,91 @@ import argparse
 import threading
 import socket
 from cipher import Cipher
-import time
+import select
+import pdb
 
 
-class Client:
+class InitFailure(Exception):
+    pass
+
+class RemoteClose(Exception):
+    pass
+
+class TimeOut(Exception):
+    pass
+
+class ClientTCPRelay:
+    TIMEOUT = 10
     BUF_SIZE = 32 * 1024
+    STAGE_STREAM = 1
+    STAGE_INIT = 0
+    def __init__(self, config, local_sock):
+        self.local_addr = config["local_addr"]
+        self.local_port = config["local_port"]
+        self.remote_addr = config["server_addr"]
+        self.remote_port = config["server_port"]
+        self.local_conn = local_sock
+        self.remote_conn = None
+        self.config = config
+        self.stage = self.STAGE_INIT
+    
+    def handle_init(self, data):
+        if data != b'\x05\x01\x00':
+            raise InitFailure
+        self.local_conn.send(b'\x05\00')
+        self.stage = self.STAGE_STREAM
+
+    def handle_stream(self, data):
+        self.remote_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.remote_conn.connect((self.remote_addr, self.remote_port))
+        except:
+            raise RemoteClose
+        self.remote_conn.send(data)
+        remote_ready = select.select([self.remote_conn], [], [], self.TIMEOUT)
+        if remote_ready[0]:
+            data = self.remote_conn.recv(self.BUF_SIZE)
+        else:
+            raise TimeOut
+
+
+    def handle_message(self, data):
+        if self.stage == self.STAGE_INIT:
+            self.handle_init(data)
+        elif self.stage == self.STAGE_STREAM:
+            self.handle_stream(data)
+
+    def close(self):
+        self.local_conn.close()
+        if self.remote_conn:
+            self.remote_conn.close() 
+
+    def run(self):
+        while True: 
+            local_ready = select.select([self.local_conn], [], [], self.TIMEOUT)
+            if local_ready[0]:
+                data = self.local_conn.recv(self.BUF_SIZE)
+            else:
+                break
+            print(data)
+            try:
+                self.handle_message(data)
+            except TimeOut:
+                print("timeout")
+                break
+            except InitFailure:
+                print("Init failed")
+                break
+            except RemoteClose:
+                print("RemoteClose")
+                break
+            except:
+                break
+        print("quit")
+        self.close()
+        
+class Client:
+    
 
     def __init__(self, config):
         self.local_addr = config["local_addr"]
@@ -20,66 +100,21 @@ class Client:
 
         self.remote_addr = config["server_addr"]
         self.remote_port = config["server_port"]
-        self.remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-
+    
         self.cipher = Cipher()
+        self.config = config
 
-        self.socks = []
-
-
-    def connect_server(self):
-        while True:
-            try:
-                self.server_socket.connect((self.server_addr,self.server_port))
-            except:
-                print("Server Offline!")
-                time.sleep(1)
+    def new_tcprelay(self, local_sock):
+        tcp = ClientTCPRelay(self.config, local_sock)
+        tcp.run()
         
-
-    def receive_local(self):
-        print('Accept from local port')
-        while True:
-            data = sock.recv(self.BUF_SIZE)
-            data = self.cipher.encrypt(data)
-            if not data:
-                break
-            try:
-                self.server_socket.send(data)
-            except OSError:
-                self.connect_server()
-                self.server_socket.send(data)
-
-        sock.close()
-        print('Connection from %s:%s closed.' % addr)
-
-    def send_server(self):
-        print('Send to server')
-
-
-    def send_local(self, sock, addr):
-        print('Accept new connection from %s:%s...' % addr)
-
-    def receive_server(self, sock, addr):
-        while True:
-            data = sock.recv(self.BUF_SIZE)
-            data = self.cipher.decrypt(data)
-            if not data:
-                break
-
 
     def loop(self):
         while True:
-
             local_sock, local_addr = self.local_socket.accept()
-            forward = threading.Thread(target=self.handle_local, args=[local_sock,local_addr])
-            server_sock, server_addr = self.server_socket.accept()
-            except OSError:
-                connect_server()
-                server_sock, server_addr = self.server_socket.accept()
-
-            backward = threading.Thread(target=self.handle_server, args=[server_sock,server_addr])
-            forward.start()
+            t = threading.Thread(target = self.new_tcprelay, args=[local_sock])
+            t.start()
+            
 
 
 
@@ -89,14 +124,9 @@ def main():
               "server_addr": "127.0.0.1",
               "server_port": 1234}
     client = Client(config)
-    client.run()
-
-    
-    connection_handler(sock, addr)
+    client.loop()
 
 
     
-
-
 if __name__ == '__main__':
     main()
--- SBUSocks/server.py
+++ SBUSocks/server.py
@@ -1,52 +1,152 @@
 import argparse
 import threading
 import socket
+from cipher import Cipher
+import select
+import pdb
 
-class Server:
+
+def int_from_bytes(bytes_object):
+    return int.from_bytes(bytes_object, byteorder='big')
+
+class ConnectionFailure(Exception):
+    pass
+
+class RemoteClose(Exception):
+    pass
+
+class TimeOut(Exception):
+    pass
+
+
+
+class SocksPacket:
+    def __init__(self, data):
+        self.data = data
+    def unpack(self):
+        self.version = self.data[0]
+        self.cmd = self.data[1]
+        self.des
+    def pack(self):
+        pass
+
+class ServerTCPRelay:
+    TIMEOUT = 10
     BUF_SIZE = 32 * 1024
+    STAGE_CONNECTION = 0
+    STAGE_STREAM = 1
+
+    def __init__(self, config, local_sock):
+        self.local_addr = config["local_addr"]
+        self.local_port = config["local_port"]
+        self.local_conn = local_sock
+        self.remote_conn = None
+        self.config = config
+        self.stage = self.STAGE_CONNECTION
+        self.version = None
+        self.server_addr = None
+        self.server_port = None
+
+    def handle_connection(self, data):
+        if data[:3] == b'\x05\x01\x00':
+            if data[3] == 0x03:
+                
+                self.target_addr = None
+                self.target_port = int_from_bytes(data[-2:])
+                print(data[3:-2], self.target_port)
+
+                #socket.gethostbyname(host)
+        else:
+            raise ConnectionFailure
+
+
+    def handle_stream(self, data):
+        socks_packet = SocksPacket(data)
+
+        self.remote_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.remote_conn.connect((self.remote_addr, self.remote_port))
+        except:
+            raise RemoteClose
+        self.remote_conn.send(data)
+        remote_ready = select.select([self.remote_conn], [], [], self.TIMEOUT)
+        if remote_ready[0]:
+            data = self.remote_conn.recv(self.BUF_SIZE)
+        else:
+            raise TimeOut
+
+
+    def handle_message(self, data):
+        if self.stage == self.STAGE_CONNECTION:
+            self.handle_connection(data)
+        else:
+            self.handle_stream(data)
+
+    def close(self):
+        self.local_conn.close()
+        if self.remote_conn is not None:
+            self.remote_conn.close() 
+
+    def run(self):
+        while True: 
+            local_ready = select.select([self.local_conn], [], [], self.TIMEOUT)
+            if local_ready[0]:
+                data = self.local_conn.recv(self.BUF_SIZE)
+            else:
+                break
+            try:
+                self.handle_message(data)
+            except TimeOut:
+                print("timeout")
+                break
+            except ConnectionFailure:
+                print("connect failed")
+                break
+            except RemoteClose:
+                print("RemoteClose")
+                break
+            except:
+                break
+        self.close()
+        print("quit")
+
+        
+class Server:
 
     def __init__(self, config):
         self.local_addr = config["local_addr"]
         self.local_port = config["local_port"]
+
         self.local_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.local_socket.bind((self.local_addr,self.local_port))
         self.local_socket.listen(1024)
         print('Listening at {}:{}'.format(self.local_addr, self.local_port))
 
+    
+        self.cipher = Cipher()
+        self.config = config
 
-    def connection_handler(self, sock, addr):
-        print('Accept new connection from %s:%s...' % addr)
-        while True:
-            data = sock.recv(self.BUF_SIZE)
-            if not data:
-                break
-            print(data)
-        sock.close()
-        print('Connection from %s:%s closed.' % addr)
+    def new_tcprelay(self, local_sock):
+        tcp = ServerTCPRelay(self.config, local_sock)
+        tcp.run()
+        
 
-    def run(self):
+    def loop(self):
         while True:
-            sock, addr = self.local_socket.accept()
-            t = threading.Thread(target=self.connection_handler, args=[sock, addr])
+            local_sock, local_addr = self.local_socket.accept()
+            t = threading.Thread(target = self.new_tcprelay, args=[local_sock])
             t.start()
-
-
-
+            
 
 
 
 def main():
-    config = {"local_addr": "127.0.0.1",
+    config = {"local_addr": "0.0.0.0",
               "local_port": 1234}
     server = Server(config)
-    server.run()
-
-    
-    connection_handler(sock, addr)
+    server.loop()
 
 
     
-
-
 if __name__ == '__main__':
     main()
